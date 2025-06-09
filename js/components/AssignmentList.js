504ac95ba491f2c9a1cc3393df09aad949929217
g-i-kala <5star@wp.pl>
2025-06-09 17:47:08 +0200

    feat: tags, tag filrer &currentTag styling

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -4,10 +4,23 @@ export default {
   components: { Assignment },
   template: `
     <section v-show="assignments.length">
-      <h2 class="font-bold mb-2">{{ title }}</h2>
+      <h2 class="font-bold mb-2">{{ title }}
+      <span>({{ assignments.length }})</span>
+      </h2>
+      <div class="flex gap-2 mb -4">
+        <button 
+          @click="currentTag = tag"
+          v-for="tag in tags" 
+          class="border border-white rounded px-2 text-xs hover:bg-white hover:text-black"
+          :class="{
+            'bg-white/20': tag === currentTag
+          }">
+          {{ tag }}
+        </button>
+      </div>
       <ul class="border border-gray-600 divide-gray-600">
         <assignment 
-          v-for="assignment in assignments" 
+          v-for="assignment in filteredAssignments" 
           :key="assignment.id"
           :assignment="assignment">
         </assignment>
@@ -17,5 +30,23 @@ export default {
   props: {
     assignments: Array,
     title: String,
+  },
+
+  data() {
+    return {
+      currentTag: 'all',
+    };
+  },
+
+  computed: {
+    tags() {
+      return ['all', ...new Set(this.assignments.map((a) => a.tag))];
+    },
+    filteredAssignments() {
+      if (this.currentTag === 'all') {
+        return this.assignments;
+      }
+      return this.assignments.filter((a) => a.tag === this.currentTag);
+    },
   },
 };
